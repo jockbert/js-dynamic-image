45a364abe664cf2faf2d0d5324c6cc936fb87b72
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-05-27 14:10:10 +0200

    Optimize source code for minification.

--- dynamicImage.js
+++ dynamicImage.js
@@ -1,41 +1,45 @@
-// Dynamic Image version 2.0
-
-function delayedCall(fn, ms) {
-    fn = fn || function () {};
-    ms = ms || 500;
-
-    var isBlocked = false;
-
-    function clearBlock() {
-        isBlocked = false;
-        fn();
-    }
-
-    return function () {
-        if (!isBlocked) {
-            isBlocked = true;
-            window.setTimeout(clearBlock, ms);
-        }
-    };
-}
-
+// Dynamic Image version 2.1
 
 function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
     widths = widths || [];
     srcs = srcs || [];
     delay = delay || 500; // half a second delay as default
-    aspectRatio = aspectRatio || 1.78; // ratio 16/9
+    aspectRatio = aspectRatio || 1.8; // ratio 16/9
 
     var elem = document.getElementById(elemId),
         currentWidth = -1,
-        image = this,
-        win = window; // minification optimization
+        image = this, // Minification optimization and reference clarification.
+        win = window; // Minification optimization.
 
+    image.setWidthAlternatives = function (ws, ss, aRatio) {
+        widths = ws;
+        srcs = ss;
+        aspectRatio = aRatio || aspectRatio;
+        currentWidth = -1;
+        image.delayedUpdate();
+    };
+
+    function updateHeight() {
+        if (currentWidth == -1) updateHeightBasedOnWidth();
+        else updateHeightDefault();
+    }
+
+    function updateHeightBasedOnWidth() {
+        var height = Math.round(elem.offsetWidth / aspectRatio);
+        elem.style.height = '' + height + 'px';
+    }
+
+    function updateHeightDefault() {
+        elem.style.height = '100%';
+    }
+    
     function isElementInViewport(el) {
-        var top = el.offsetTop;
-        var left = el.offsetLeft;
-        var width = el.offsetWidth;
-        var height = el.offsetHeight;
+        var top = el.offsetTop,
+            left = el.offsetLeft,
+            width = el.offsetWidth,
+            height = el.offsetHeight,
+            pageXOffset = win.pageXOffset, // Minification optimization.
+            pageYOffset = win.pageYOffset; // Minification optimization.
 
         while (el.offsetParent) {
             el = el.offsetParent;
@@ -44,41 +48,15 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
         }
 
         return (
-            top < (win.pageYOffset + win.innerHeight) &&
-            left < (win.pageXOffset + win.innerWidth) &&
-            (top + height) > win.pageYOffset &&
-            (left + width) > win.pageXOffset
+            top < (pageYOffset + win.innerHeight) &&
+            left < (pageXOffset + win.innerWidth) &&
+            (top + height) > pageYOffset &&
+            (left + width) > pageXOffset
         );
     }
 
-    this.setWidthAlternatives = function (ws, ss, aRatio) {
-        widths = ws;
-        srcs = ss;
-        aspectRatio = aRatio || aspectRatio;
-        currentWidth = -1;
-        this.delayedUpdate();
-    };
-
-    function updateHeight(elem) {
-        if (isNotLoadedYet()) updateHeightBasedOnWidth(elem);
-        else updateHeightDefault(elem);
-    }
-
-    function updateHeightBasedOnWidth(elem) {
-        var height = Math.round(elem.offsetWidth / aspectRatio);
-        elem.style.height = '' + height + 'px';
-    }
-
-    function updateHeightDefault(elem) {
-        elem.style.height = '100%';
-    }
-
-    function isNotLoadedYet() {
-        return currentWidth == -1;
-    }
-
-    this.update = function () {
-        updateHeight(elem);
+    image.update = function () {
+        updateHeight();
         if (!isElementInViewport(elem)) return;
 
         var elemWidth = elem.offsetWidth;
@@ -93,17 +71,33 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
         }
 
         if (width > currentWidth) {
+            console.log("source change to " + src);
             elem.src = src;
             currentWidth = width;
         }
     };
 
-    this.delayedUpdate = delayedCall(this.update, delay);
+    function delayedCall(fn, ms) {
+        var isBlocked = false;
 
+        function clearBlock() {
+            isBlocked = false;
+            fn();
+        }
 
-    this.selfRegister = function () {
-        var nestedOnresize = win.onresize;
-        var nestedOnscroll = win.onscroll;
+        return function () {
+            if (!isBlocked) {
+                isBlocked = true;
+                win.setTimeout(clearBlock, ms);
+            }
+        };
+    }
+
+    image.delayedUpdate = delayedCall(image.update, delay);
+
+    image.selfRegister = function () {
+        var nestedOnresize = win.onresize,
+            nestedOnscroll = win.onscroll;
 
         win.onresize = function () {
             if (nestedOnresize) nestedOnresize();
@@ -117,6 +111,9 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
 
     // --- creation preparations ----
     elem.style.width = '100%';
-    elem.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACAQMAAABIeJ9nAAAAA1BMVEWgoKAG03+7AAAACklEQVQI12MAAgAABAABINItbwAAAABJRU5ErkJggg==";
-    updateHeightBasedOnWidth(elem);
+    // gif version
+    elem.src="data:image/gif;base64,R0lGODlhAQABAIABAKCgoP///yH5BAEKAAEALAAAAAABAAEAAAICRAEAOw==";
+    //png version 
+    // elem.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACAQMAAABIeJ9nAAAAA1BMVEWgoKAG03+7AAAACklEQVQI12MAAgAABAABINItbwAAAABJRU5ErkJggg==";
+    updateHeightBasedOnWidth();
 }
