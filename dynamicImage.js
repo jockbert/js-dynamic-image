0bff38f0beaddca908ce88e330637500ad9f317c
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-05-27 15:24:34 +0200

    Minification optimizations including interface changes.

--- dynamicImage.js
+++ dynamicImage.js
@@ -1,4 +1,4 @@
-// Dynamic Image version 2.2
+// Dynamic Image version 2.3
 
 function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
     widths = widths || [];
@@ -11,19 +11,19 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
         image = this, // Minification optimization and reference clarification.
         win = window; // Minification optimization.
 
-    image.setWidthAlternatives = function (ws, ss, aRatio) {
+    image.setWidths = function (ws, ss, aRatio) {
         widths = ws;
         srcs = ss;
         aspectRatio = aRatio || aspectRatio;
         currentWidth = -1;
-        image.delayedUpdate();
+        delayedUpdateFn();
     };
 
     function updateHeight() {
         var height = currentWidth == -1 ? Math.round(elem.offsetWidth / aspectRatio) + 'px' : '100%';
         if (elem.style.height != height) {
             elem.style.height = height;
-        };
+        }
     }
 
     function isElementInViewport(el) {
@@ -48,7 +48,7 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
         );
     }
 
-    image.update = function () {
+    function update() {
         updateHeight();
         if (!isElementInViewport(elem)) return;
 
@@ -68,7 +68,7 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
             currentWidth = width;
             updateHeight();
         }
-    };
+    }
 
     function delayedCall(fn, ms) {
         var isBlocked = false;
@@ -86,21 +86,23 @@ function DynamicImage(elemId, delay, widths, srcs, aspectRatio) {
         };
     }
 
-    image.delayedUpdate = delayedCall(image.update, delay);
+    var delayedUpdateFn = delayedCall(update, delay);
 
-    image.selfRegister = function () {
-        var nestedOnresize = win.onresize,
-            nestedOnscroll = win.onscroll;
+    image.delayedUpdate = delayedUpdateFn;
 
-        win.onresize = function () {
-            if (nestedOnresize) nestedOnresize();
-            image.delayedUpdate();
-        };
-        win.onscroll = function () {
-            if (nestedOnscroll) nestedOnscroll();
-            image.delayedUpdate();
-        };
+    image.register = function () {
+
+        function callOldAndUpdate(oldFn) {
+            return function () {
+                if (oldFn) oldFn();
+                delayedUpdateFn();
+            };
+        }
+
+        win.onresize = callOldAndUpdate(win.onresize);
+        win.onscroll = callOldAndUpdate(win.onscroll);
     };
+  
 
     // --- creation preparations ----
     elem.style.width = '100%';
